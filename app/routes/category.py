a870eb3e1e71b20cdcbf08c11ecd998ad568b57d
alice <_@email.com>
2024-04-23 17:20:22 +0300

    add api create category

--- app/routes/category.py
+++ app/routes/category.py
@@ -1,4 +1,14 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from slugify import slugify
+from sqlalchemy import insert
+from sqlalchemy.orm import Session
+from starlette import status
+
+from app.backend.db_depends import get_db
+from app.models import Category
+from app.schemas import CreateCategory
 
 router = APIRouter(prefix='/category', tags=['category'])
 
@@ -9,8 +19,18 @@ async def get_all_categories():
 
 
 @router.post('/create_category')
-async def create_category():
-    pass
+async def create_category(db: Annotated[Session, Depends(get_db)], category: CreateCategory):
+    query = insert(Category).values(
+        name=category.name,
+        parent_id=category.parent_id,
+        slug=slugify(category.name)
+    )
+    db.execute(query)
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
 
 
 @router.put('/update_category')
