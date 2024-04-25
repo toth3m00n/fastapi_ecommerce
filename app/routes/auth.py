7bdd78fae8d41b3a959a5306bd0333d852be9774
alice <_@email.com>
2024-04-25 11:30:18 +0300

    add oauth2

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,7 +1,15 @@
+import os
+from datetime import datetime, timedelta
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import (
+    HTTPBasic,
+    HTTPBasicCredentials,
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestForm,
+)
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -10,20 +18,27 @@ from starlette import status
 from app.backend.db_depends import get_db
 from app.models.user import User
 from app.schemas import CreateUser
+from dotenv import load_dotenv
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 security = HTTPBasic()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+load_dotenv('app/.env')
 
-@router.post('/')
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+
+@router.post("/")
 async def create_user(db: Annotated[AsyncSession, Depends(get_db)], user: CreateUser):
     query = insert(User).values(
         first_name=user.first_name,
         last_name=user.last_name,
         username=user.username,
         email=user.email,
-        hashed_password=bcrypt_context.hash(user.password)
+        hashed_password=bcrypt_context.hash(user.password),
     )
 
     await db.execute(query)
@@ -32,13 +47,117 @@ async def create_user(db: Annotated[AsyncSession, Depends(get_db)], user: Create
     return {"status_code": status.HTTP_201_CREATED, "transaction": "successful"}
 
 
-async def get_current_username(db: Annotated[AsyncSession, Depends(get_db)], credentials: HTTPBasicCredentials = Depends(security)):
-    user = await db.scalar(select(User).where(User.username == credentials.username))
-    if not user or not bcrypt_context.verify(credentials.password, user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+# async def get_current_username(db: Annotated[AsyncSession, Depends(get_db)], credentials: HTTPBasicCredentials = Depends(security)):
+#     user = await db.scalar(select(User).where(User.username == credentials.username))
+#     if not user or not bcrypt_context.verify(credentials.password, user.hashed_password):
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+#     return user
+#
+#
+
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        is_admin: str = payload.get('is_admin')
+        is_supplier: str = payload.get('is_supplier')
+        is_customer: str = payload.get('is_customer')
+        expire = payload.get('exp')
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Could not validate user'
+            )
+        if expire is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No access token supplied"
+            )
+        if datetime.now() > datetime.fromtimestamp(expire):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Token expired!"
+            )
+
+        return {
+            'username': username,
+            'id': user_id,
+            'is_admin': is_admin,
+            'is_supplier': is_supplier,
+            'is_customer': is_customer,
+        }
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Could not validate user'
+        )
+
+
+@router.get('/read_current_user')
+def read_current_user(user: User = Depends(get_current_user)):
+    return {'User': user}
+
+
+async def authenticate_user(
+    db: Annotated[AsyncSession, Depends(get_db)], username: str, password: str
+):
+    user = await db.scalar(select(User).where(User.username == username))
+
+    if (
+        not user
+        or not bcrypt_context.verify(password, user.hashed_password)
+        or user.is_active == False
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
 
 
-@router.get('/users/me')
-def read_current_user(user: str = Depends(get_current_username)):
-    return {'User': user}
+@router.post("/token")
+async def login(
+    db: Annotated[AsyncSession, Depends(get_db)],
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+):
+    user = await authenticate_user(db, form_data.username, form_data.password)
+
+    if not user or user.is_active == False:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user"
+        )
+
+    token = await create_access_token(
+        user.username,
+        user.id,
+        user.is_admin,
+        user.is_supplier,
+        user.is_customer,
+        expires_delta=timedelta(minutes=20),
+    )
+
+    return {"access_token": token, "token_type": "bearer"}
+
+
+async def create_access_token(
+    username: str,
+    user_id: int,
+    is_admin: bool,
+    is_supplier: bool,
+    is_customer: bool,
+    expires_delta: timedelta,
+):
+    encode = {
+        "sub": username,
+        "id": user_id,
+        "is_admin": is_admin,
+        "is_supplier": is_supplier,
+        "is_customer": is_customer,
+    }
+    expires = datetime.now() + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
