dd5338ccb2981bc2bc3c4ff0da6b12c2ff951db8
alice <_@email.com>
2024-06-10 16:21:51 +0300

    add logger

--- app/main.py
+++ app/main.py
@@ -1,11 +1,32 @@
-from fastapi import FastAPI
+from uuid import uuid4
+from loguru import logger
+
+from fastapi import FastAPI, Request
+from starlette.responses import JSONResponse
 
 from app.middlewares import TimingMiddleware
 from app.routes import category, product, auth, permission
 
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message} ", level="INFO", enqueue=True)
+
 app = FastAPI(version='0.0.1')
 app.add_middleware(TimingMiddleware)
 
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
 
 @app.get("/")
 async def welcome() -> dict:
